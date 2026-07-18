7a87a78899c5678bb68668b9f73dd0fece846b40
Joelle Kaufman <69372896+Jo5687@users.noreply.github.com>
2026-07-17 18:06:37 -0700

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        //Edge case
+        if(s.length() == 0)
+        {
+            return 0;
+        }
+        
+        int maxLength {1};
+        int length {1};
+        int left {0};
+        unordered_map<char, int> seen;
+
+        for(int right = 0; right < s.length(); right++)
+        {
+            //Slide window left
+            while(seen.contains(s[right]))
+            {
+                seen.erase(s[left]);
+                left += 1;
+            }
+            
+            //Expand window
+            seen[s[right]]++;
+            length = right - left + 1; 
+            if(length > maxLength)
+            {
+                maxLength = length;
+            }
+        }
+        return maxLength;
+    }
+};
